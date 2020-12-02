4cf846a39456a877b08c9530599bf1c2efaf90f5
0vercl0k <1476421+0vercl0k@users.noreply.github.com>
2020-12-01 16:27:33 -0800

    init

---
+++ build/build-msvc.bat
@@ -0,0 +1,3 @@
+REM Axel '0vercl0k' Souchet - December 1 2020
+cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo
+cmake --build .
---
+++ src/clairvoyance.cc
@@ -0,0 +1,6 @@
+// Axel '0vercl0k' Souchet - December 1 2020
+#include <cstdio>
+
+int main() {
+    return 0;
+}
